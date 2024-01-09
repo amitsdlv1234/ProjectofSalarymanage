eae0a53548e385acf817bc06021b091f236f7739
Amit Kumar <115470270+amitsdlv1234@users.noreply.github.com>
2024-01-09 19:27:33 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Amit@1234',
-    database: 'IntituteProject',
+    password: 'your_MySql_password',
+    database: 'your_databseName',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
